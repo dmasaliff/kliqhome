06b5583ef41be45c796d820829e59f0175ed0f12
dmasaliff <dimasalifferdiansyah@gmail.com>
2026-03-28 16:04:54 +0700

    gemini flash latest

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -10,7 +10,7 @@ export async function POST(req: Request) {
     const { message } = await req.json();
 
     const model = genAI.getGenerativeModel({ 
-      model: "gemini-1.5-flash",
+      model: "gemini-1.5-flash-latest",
       
      systemInstruction: `
         Kamu adalah KLIQ AI, asisten servis AC jujur dari Jabodetabek.
